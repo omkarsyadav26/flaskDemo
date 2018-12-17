8372f1d6e06ba02eceac0c887cb52728bff04929
Omkar Yadav <omkar_yadav@persistent.co.in>
2018-12-17 13:53:36 +0530

    commited with new index form page

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 posts = [
@@ -17,9 +17,15 @@ posts = [
 ]
 
 @app.route("/")
-@app.route("/home")
+def index():
+    return render_template("index.html", title="index")
+@app.route("/home", methods=["GET","POST"])
 def home():
-    return render_template("home.html", posts=posts, title="home")
+    if request.method == "GET":
+        return "Please submit your name first"
+    else:
+        name = request.form.get("name")
+        return render_template("home.html", posts=posts, title="home", name=name)
 @app.route("/about")
 def about():
     return render_template('about.html', title="about")
